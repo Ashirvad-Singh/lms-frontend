7113e103968e7d58336d956761c412d5d7ddb756
ashirvad2912@gmail.com <ashirvad2912@gmail.com>
2024-05-20 10:04:29 +0530

    first commit

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -7,7 +7,7 @@ const CourseCard = ({ data }) => {
   return (
     <div
       onClick={() => navigate("/course/description", { state: { ...data } })}
-      className="text-white w-[22rem] h-[430px] shadow-lg rounded-lg cursor-pointer group overflow-hidden bg-zinc-700"
+      className= " bg-yellow-500 text-blue-800 w-[22rem] h-[430px] shadow-lg rounded-lg cursor-pointer group overflow-hidden "
     >
       <div className="overflow-hidden">
         <img
@@ -18,21 +18,21 @@ const CourseCard = ({ data }) => {
       </div>
 
       {/* course details */}
-      <div className="p-3 space-y-1 text-white">
+      <div className="p-3 space-y-1 text-blue-800">
         <h2 className="text-xl font-bold text-yellow-500 line-clamp-2">
           {data?.title}
         </h2>
         <p className="line-clamp-2">{data?.description}</p>
         <p className="font-semibold">
-          <span className="text-yellow-500 font-bold">Category : </span>
+          <span className="text-blue-800 font-bold">Category : </span>
           {data?.category}
         </p>
         <p className="font-semibold">
-          <span className="text-yellow-500 font-bold">Total Lectures : </span>
+          <span className="text-blue-800 font-bold">Total Lectures : </span>
           {data?.numberOfLectures}
         </p>
         <p className="font-semibold">
-          <span className="text-yellow-500 font-bold">Instructor : </span>
+          <span className="text-blue-800 font-bold">Instructor : </span>
           {data?.createdBy}
         </p>
       </div>
--- src/Pages/Course/CourseList.js
+++ src/Pages/Course/CourseList.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import CourseCard from "../../Components/CourseCard";
 import Layout from "../../Layout/Layout";
 import { getAllCourses } from "../../Redux/courseSlice";
-
+import Tilt from 'react-parallax-tilt';
 const Courses = () => {
   const dispatch = useDispatch();
   const { coursesData } = useSelector((state) => state.course);
@@ -17,7 +17,7 @@ const Courses = () => {
   return (
     <Layout>
       {/* courses container for displaying the cards */}
-      <div className="min-h-[90vh] pt-12 pl-20 flex flex-col flex-wrap gap-10 text-white">
+      <div className="min-h-[90vh] pt-12 pl-20 flex flex-col flex-wrap gap-10 text-white bg-blue-900">
         <h1 className="text-center text-3xl font-semibold">
           Explore the courses made by{" "}
           <span className="font-bold text-yellow-500">Industry Experts</span>
@@ -26,7 +26,7 @@ const Courses = () => {
         {/* wrapper for courses card */}
         <div className="mb-10 flex flex-wrap gap-14">
           {coursesData?.map((element) => {
-            return <CourseCard key={element._id} data={element} />;
+            return   <Tilt><CourseCard key={element._id} data={element} /></Tilt>
           })}
         </div>
       </div>
